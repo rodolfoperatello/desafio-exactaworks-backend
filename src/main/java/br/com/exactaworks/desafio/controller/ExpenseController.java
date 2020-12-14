957329ae9bd7d2c6e5d2eb3be72734403917829b
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-14 11:07:20 -0300

    Creating createExpense endpoint

--- src/main/java/br/com/exactaworks/desafio/controller/ExpenseController.java
+++ src/main/java/br/com/exactaworks/desafio/controller/ExpenseController.java
@@ -1,5 +1,8 @@
 package br.com.exactaworks.desafio.controller;
 
+import br.com.exactaworks.desafio.service.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/expense")
 public class ExpenseController {
 
-    public createExpense(@RequestBody @Validated ExpenseR)
+    private final ExpenseService expenseService;
 
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    public ResponseEntity<ExpenseResponse> createExpense(@RequestBody @Validated ExpenseRequest expenseRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.expenseService.saveExpense(expenseRequest));
+    }
 
 }
