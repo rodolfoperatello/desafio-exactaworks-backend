8d34d409a333c1b002716767cb1b06d422956b08
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-14 13:16:59 -0300

    Creating ExpenseMapper class

---
+++ src/main/java/br/com/exactaworks/desafio/mapper/ExpenseMapper.java
@@ -0,0 +1,29 @@
+package br.com.exactaworks.desafio.mapper;
+
+import br.com.exactaworks.desafio.builder.request.ExpenseBuilderEntity;
+import br.com.exactaworks.desafio.builder.response.ExpenseBuilderResponse;
+import br.com.exactaworks.desafio.controller.request.ExpenseRequest;
+import br.com.exactaworks.desafio.controller.response.ExpenseResponse;
+import br.com.exactaworks.desafio.entity.ExpenseEntity;
+
+public class ExpenseMapper {
+
+    public static ExpenseEntity convertToEntity(ExpenseRequest expenseRequest){
+        return new ExpenseBuilderEntity()
+                .withName(expenseRequest.getName())
+                .withDescription(expenseRequest.getDescription())
+                .withValue(expenseRequest.getValue())
+                .build();
+    }
+
+    public static ExpenseResponse convertToResponse(ExpenseEntity expenseEntity){
+        return new ExpenseBuilderResponse()
+                .withId(expenseEntity.getId())
+                .withName(expenseEntity.getName())
+                .withDescription(expenseEntity.getDescription())
+                .withLocalDateTime(expenseEntity.getDateTime())
+                .withValue(expenseEntity.getValue())
+                .withTag(expenseEntity.getTag())
+                .build();
+    }
+}
