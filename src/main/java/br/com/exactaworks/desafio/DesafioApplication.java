54e18ed9f125ca694640029d9ffabb820784c7ed
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-14 09:54:26 -0300

    Creating project structure

---
+++ src/main/java/br/com/exactaworks/desafio/DesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.exactaworks.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
