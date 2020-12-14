143a4468cc19da17352bb6cd6c8e5bebce64391c
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-14 15:30:18 -0300

    Inserting @Repository annotation in ExpenseRepository class

--- src/main/java/br/com/exactaworks/desafio/repository/ExpenseRepository.java
+++ src/main/java/br/com/exactaworks/desafio/repository/ExpenseRepository.java
@@ -2,6 +2,8 @@ package br.com.exactaworks.desafio.repository;
 
 import br.com.exactaworks.desafio.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
 }
