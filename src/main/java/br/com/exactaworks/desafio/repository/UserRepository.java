a29cbf9e745ec2b1e17d24e3523f1f5f7c840481
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-29 14:25:42 -0300

    Creating UserRepository

---
+++ src/main/java/br/com/exactaworks/desafio/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.exactaworks.desafio.repository;
+
+import br.com.exactaworks.desafio.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
