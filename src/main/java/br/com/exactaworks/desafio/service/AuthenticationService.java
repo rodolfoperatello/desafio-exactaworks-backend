056dd2e965cb0c931e992aaa970b6191f06fe9ee
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-29 14:25:18 -0300

    Creating AuthenticationService

---
+++ src/main/java/br/com/exactaworks/desafio/service/AuthenticationService.java
@@ -0,0 +1,22 @@
+package br.com.exactaworks.desafio.service;
+
+import br.com.exactaworks.desafio.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Dados inválidos"));
+    }
+}
