653ea23f154d75f0c07edfe2dfbcd96a91aaeae7
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-29 10:13:04 -0300

    Creating Perfil and User class

---
+++ src/main/java/br/com/exactaworks/desafio/entity/Perfil.java
@@ -0,0 +1,47 @@
+package br.com.exactaworks.desafio.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Perfil() {
+
+    }
+
+    public Perfil(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    //atributo que tem o nome do perfil
+    public String getAuthority() {
+        return this.name;
+    }
+}
