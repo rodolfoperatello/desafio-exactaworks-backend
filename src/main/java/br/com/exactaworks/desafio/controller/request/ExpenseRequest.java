56fb3aec9f9162753d1e20434988cc4714b2af88
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-30 14:33:59 -0300

    Inserting @Email annotation in ExpenseRequest class

--- src/main/java/br/com/exactaworks/desafio/controller/request/ExpenseRequest.java
+++ src/main/java/br/com/exactaworks/desafio/controller/request/ExpenseRequest.java
@@ -1,5 +1,6 @@
 package br.com.exactaworks.desafio.controller.request;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 public class ExpenseRequest {
 
     @NotBlank(message = "O nome do usuário não pode ser nulo ou em branco")
+    @Email(message = "Informar um email válido Ex: user@email.com")
     private String name;
     @NotBlank(message = "A descrição da despesa não pode ser nulo ou em branco")
     private String description;
