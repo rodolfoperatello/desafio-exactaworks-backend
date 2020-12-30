92abbdc66a9cf25e83d0a8e3574f0bec8cc7f842
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-30 14:34:36 -0300

    Creating FindByEmail and findUserById methods in UserRepository

--- src/main/java/br/com/exactaworks/desafio/repository/UserRepository.java
+++ src/main/java/br/com/exactaworks/desafio/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findUserById(Long userId);
 }
