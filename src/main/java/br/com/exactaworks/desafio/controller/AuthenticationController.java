5682a27f1f9a367af9cf2d627f08d8774cecdb9c
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-12-30 14:35:21 -0300

    Creating AuthenticationController class

---
+++ src/main/java/br/com/exactaworks/desafio/controller/AuthenticationController.java
@@ -0,0 +1,48 @@
+package br.com.exactaworks.desafio.controller;
+
+import br.com.exactaworks.desafio.controller.request.LoginRequest;
+import br.com.exactaworks.desafio.controller.response.TokenResponse;
+import br.com.exactaworks.desafio.mapper.LoginMapper;
+import br.com.exactaworks.desafio.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "/api/auth")
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final Logger log = Logger.getLogger("br.com.exactaworks.desafio.AuthenticationController");
+
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> authenticate(@RequestBody @Validated LoginRequest loginRequest){
+
+        try {
+            var authentication = authenticationManager.authenticate(
+                    LoginMapper.toUsernamePasswordAuthenticationToken(loginRequest));
+            var token = tokenService.createToken(authentication);
+            log.info("Token: " +token);
+
+            return ResponseEntity.status(HttpStatus.OK).body(new TokenResponse(token, "Bearer"));
+        } catch (AuthenticationException authenticationException) {
+
+            log.info("BAD REQUEST");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+}
