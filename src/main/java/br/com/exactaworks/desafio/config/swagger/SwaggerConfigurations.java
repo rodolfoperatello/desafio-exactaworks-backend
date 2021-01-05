cafac07c927fa57a3c1817134a6dd3358632a987
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2021-01-05 12:07:27 -0300

    Update in SwaggerConfiguration class

--- src/main/java/br/com/exactaworks/desafio/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/exactaworks/desafio/config/swagger/SwaggerConfigurations.java
@@ -1,23 +1,59 @@
 package br.com.exactaworks.desafio.config.swagger;
 
-import br.com.exactaworks.desafio.entity.UserEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
-public class SwaggerConfigurations {
+@EnableSwagger2
+public class SwaggerConfigurations{
 
     @Bean
-    public Docket expenseApi(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.exactaworks.desafio"))
-                .paths(PathSelectors.ant("/**"))
+                .paths(PathSelectors.ant("/api/**"))
                 .build()
-                .ignoredParameterTypes(UserEntity.class);
+                .ignoredParameterTypes(
+                        org.springframework.data.domain.Pageable.class,
+                        Page.class,
+                        Sort.class)
+                .directModelSubstitute(Pageable.class, SwaggerPageable.class)
+                .tags(new Tag("Authentication Controller", "Endpoints in Authentication Controller"),
+                        new Tag("Expense Controller", "Endpoints in Expense Controller"),
+                        new Tag("User Controller", "Endpoints in User Controller"))
+                .globalOperationParameters(Collections.singletonList(new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Header for JWT Token")
+                        .modelRef(new ModelRef("String"))
+                        .parameterType("header")
+                        .required(false)
+                        .build()))
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo("Expense API",
+                "API to control personal expenses developed for Exactaworks LTDA",
+                "0.0.1",
+                "Free to use",
+                new Contact("Rodolfo Peratello", "", "rodolfo.peratello@exactaworks.com.br"),
+                "", "", Collections.emptyList());
     }
 }
+
