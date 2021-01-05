ed430bf6066800400dd933a2ba77665695fadc09
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2021-01-05 17:19:02 -0300

    Renaming GenericExceptionHandler to RestExceptionHandler

--- src/main/java/br/com/exactaworks/desafio/exceptions/handler/RestExceptionHandler.java
+++ src/main/java/br/com/exactaworks/desafio/exceptions/handler/RestExceptionHandler.java
@@ -1,45 +1,41 @@
 package br.com.exactaworks.desafio.exceptions.handler;
 
-import br.com.exactaworks.desafio.exceptions.errorResponses.ErrorResponse;
 import br.com.exactaworks.desafio.exceptions.exception.NotFoundException;
+import br.com.exactaworks.desafio.exceptions.exceptiondetails.NotFoundExceptionDetails;
+import br.com.exactaworks.desafio.exceptions.exceptiondetails.MethodArgumentNotValidExceptionDetails;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler {
+
+    private final Logger logger = Logger.getLogger("br.com.exactaworks.desafio.exceptions.exceptiondetails");
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ErrorResponse handleGenericException(NotFoundException notFoundException){
-        return new ErrorResponse(notFoundException.getMessage());
+    public NotFoundExceptionDetails handleNotFoundException(NotFoundException notFoundException){
+        return new NotFoundExceptionDetails("Not Found Exception", HttpStatus.NOT_FOUND.value(), notFoundException.getMessage());
     }
 
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ObjectError> handleIllegalArgumentException(MethodArgumentNotValidException methodArgumentNotValidException){
-        List<ObjectError> allErrors = methodArgumentNotValidException.getAllErrors();
+    public List<MethodArgumentNotValidExceptionDetails> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException methodArgumentNotValidException){
 
-
-        return new ArrayList<>(methodArgumentNotValidException.getAllErrors());
+        return methodArgumentNotValidException
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> new MethodArgumentNotValidExceptionDetails(
+                        fieldError.getField(), fieldError.getDefaultMessage())).collect(Collectors.toList());
     }
-
-//    @ResponseBody
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public void handleIllegalArgumentException(IllegalArgumentException illegalArgumentException){
-//        return illegalArgumentException.get
-//    }
-
-
 }
