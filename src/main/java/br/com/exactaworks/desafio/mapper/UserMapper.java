113f16dcefb69ae7bb92e722af4473015f3467f9
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2021-01-05 11:55:56 -0300

    Creating UserMapper class

---
+++ src/main/java/br/com/exactaworks/desafio/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package br.com.exactaworks.desafio.mapper;
+
+import br.com.exactaworks.desafio.builder.request.UserBuilderEntity;
+import br.com.exactaworks.desafio.builder.response.UserBuilderResponse;
+import br.com.exactaworks.desafio.controller.request.UserRequest;
+import br.com.exactaworks.desafio.controller.response.UserResponse;
+import br.com.exactaworks.desafio.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserMapper {
+
+    public static UserEntity convertToEntity (UserRequest userRequest){
+        return new UserBuilderEntity()
+                .withName(userRequest.getName())
+                .withLastName(userRequest.getLastName())
+                .withEmail(userRequest.getEmail())
+                .withPassword(new BCryptPasswordEncoder().encode(userRequest.getPassword()))
+                .build();
+    }
+
+    public static UserResponse convertToResponse(UserEntity userEntity){
+        return new UserBuilderResponse()
+                .withFullName(userEntity.getName().concat(" ").concat(userEntity.getLastName()))
+                .withEmail(userEntity.getEmail())
+                .build();
+    }
+
+    public static Page<UserResponse> convertToPageUserResponse(Page<UserEntity> pageUserEntity) {
+        return pageUserEntity.map(UserMapper::convertToResponse);
+    }
+}
