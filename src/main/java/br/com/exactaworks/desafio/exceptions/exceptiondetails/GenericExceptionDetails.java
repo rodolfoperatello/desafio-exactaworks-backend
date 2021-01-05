0e86abae4230c4575034c2fe1b334584055c9eab
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2021-01-05 19:01:06 -0300

    Creating GenericExceptionDetails class

--- src/main/java/br/com/exactaworks/desafio/exceptions/exceptiondetails/GenericExceptionDetails.java
+++ src/main/java/br/com/exactaworks/desafio/exceptions/exceptiondetails/GenericExceptionDetails.java
@@ -1,2 +1,33 @@
-package br.com.exactaworks.desafio.exceptions.exceptiondetails;public class GenericExceptionDetails {
+package br.com.exactaworks.desafio.exceptions.exceptiondetails;
+
+import java.time.LocalDateTime;
+
+public class GenericExceptionDetails {
+
+    private final String title;
+    private final int status;
+    private final String details;
+    private final LocalDateTime localDateTime = LocalDateTime.now();
+
+    public GenericExceptionDetails(String title, int status, String details) {
+        this.title = title;
+        this.status = status;
+        this.details = details;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
 }
