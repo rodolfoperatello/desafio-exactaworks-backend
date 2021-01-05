4f6a2e30553e5a212db47bee41e7ee8b8471e436
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2021-01-05 11:56:36 -0300

    Update in UserRepository

--- src/main/java/br/com/exactaworks/desafio/repository/UserRepository.java
+++ src/main/java/br/com/exactaworks/desafio/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package br.com.exactaworks.desafio.repository;
 
 import br.com.exactaworks.desafio.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
 
     Optional<UserEntity> findUserById(Long userId);
+
+    Page<UserEntity> findAll(Pageable pageable);
 }
