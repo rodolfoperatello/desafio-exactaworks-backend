5a487a01111c96eb3589d2a4844b51a1a85b5769
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2021-01-09 11:45:49 -0300

    Creating createExpenseEntity method and findExpenseById

--- src/main/java/br/com/exactaworks/desafio/service/ExpenseService.java
+++ src/main/java/br/com/exactaworks/desafio/service/ExpenseService.java
@@ -2,6 +2,7 @@ package br.com.exactaworks.desafio.service;
 
 import br.com.exactaworks.desafio.controller.request.ExpenseRequest;
 import br.com.exactaworks.desafio.controller.response.ExpenseResponse;
+import br.com.exactaworks.desafio.entity.ExpenseEntity;
 import br.com.exactaworks.desafio.exceptions.exception.NotFoundException;
 import br.com.exactaworks.desafio.mapper.ExpenseMapper;
 import br.com.exactaworks.desafio.repository.ExpenseRepository;
@@ -13,13 +14,15 @@ import org.springframework.stereotype.Service;
 public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
+    private final UserService userService;
 
-    public ExpenseService(ExpenseRepository expenseRepository) {
+    public ExpenseService(ExpenseRepository expenseRepository, UserService userService) {
         this.expenseRepository = expenseRepository;
+        this.userService = userService;
     }
 
     public ExpenseResponse saveExpense(ExpenseRequest expenseRequest) {
-        return ExpenseMapper.convertToResponse(this.expenseRepository.save(ExpenseMapper.convertToEntity(expenseRequest)));
+        return ExpenseMapper.convertToResponse(this.expenseRepository.save(createExpenseEntity(expenseRequest)));
     }
 
     public ExpenseResponse findExpenseById(Long id) {
@@ -29,5 +32,14 @@ public class ExpenseService {
 
     public Page<ExpenseResponse> findAllExpenses(Pageable pageable) {
         return ExpenseMapper.convertToPageResponse(this.expenseRepository.findAll(pageable));
+    }
+
+    private ExpenseEntity createExpenseEntity (ExpenseRequest expenseRequest){
+        var user = this.userService.findUserEntityById(expenseRequest.getUserId());
+
+        var expenseEntity = ExpenseMapper.convertToEntity(expenseRequest);
+        expenseEntity.setUserEntity(user);
+
+        return expenseEntity;
     }
 }
